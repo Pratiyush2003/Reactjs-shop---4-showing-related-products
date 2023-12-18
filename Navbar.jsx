4d9c392127341fc417345800cde8f152e93c616e
Pratiyush2003 <152852563+Pratiyush2003@users.noreply.github.com>
2023-12-18 13:48:42 +0530

    Add files via upload

---
+++ Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <header>
+        <div className="nav-bar">
+                    <Link to={'/'} className="brand">
+                            E-cart
+                    </Link>
+                    <div className="search-bar">
+                            <input type="text" placeholder='Enter Products'/>
+                    </div>
+                    <Link to={'/Cart'} className="cart">
+                            cart
+                    </Link>
+        </div>
+        <div className="nav-bar-wrapper">
+            <div className="items">Filter by -{'>'}</div>
+            <div className="items">No Filter</div>
+            <div className="items">Mobiles</div>
+            <div className="items">Laptops</div>
+            <div className="items">Tablets</div>
+            <div className="items">{'>'}=29999</div>
+            <div className="items">{'>'}=49999</div>
+            <div className="items">{'>'}=69999</div>
+            <div className="items">{'>'}=89999</div>
+        </div>
+      
+    </header>
+  )
+}
+
+export default Navbar
